0ff339a1c6d4542c44ed4502127bc18bc05e7e08
anamikagawandi <anamika.gawandi@gmail.com>
2020-08-20 02:29:00 +0530

    user and auth middleware complete

--- server/routes/v1/movies/index.js
+++ server/routes/v1/movies/index.js
@@ -1,16 +1,17 @@
 "use strict";
 
 const routes = require("express").Router({ mergeParams: true });
-const movie = require("./logic")
+const movie = require("./logic");
+const auth = require("../../../middleware/verifyToken")
 
 module.exports = (services) => {
 
     routes.get("/", movie.listMovies(services));
     routes.get("/:id", movie.getMovieDetails(services));
     //Authotized, need an auth middleware
-    routes.post("/", movie.addMovie(services));
-    routes.put("/:id", movie.updateMovie(services));
-    routes.delete("/:id", movie.deleteMovie(services));
+    routes.post("/", auth.verifyToken(), movie.addMovie(services));
+    routes.put("/:id", auth.verifyToken(), movie.updateMovie(services));
+    routes.delete("/:id", auth.verifyToken(), movie.deleteMovie(services));
 
     return routes;
 };
--- server/routes/v1/users/logic.js
+++ server/routes/v1/users/logic.js
@@ -1,26 +1,52 @@
 "use strict"
 
-const getUserDetails = () => {
-    return (req, res, next) => {
-        res.status(200).json({ "message": "This is getUserDetails" })
-    };
+const mongodb = require("../../../helper/mongodb");
+const md5 = require("md5");
+const jwt = require('jsonwebtoken')
+
+const getToken = (services) => {
+    return async (req, res, next) => {
+        try {
+            if (req.headers && req.headers.username && req.headers.password) {
+                let hash = md5(req.headers.password)
+                let filter = {
+                    username: req.headers.username,
+                    password: hash,
+                    is_active: true
+                }
+                let isValid = await mongodb.ifExists(services.db, "users", filter)
+                if (isValid) {
+                    let token = jwt.sign({
+                        data: req.headers.email
+                    }, "secret", { expiresIn: "1h" });
+                    res.status(200).send({ token: token });
+                } else {
+                    res.status(401).json({ message: "Invalid User" })
+                }
+            } else {
+                res.status(400).json({ message: "Headers Missing" })
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
 }
 
-const updateUser = () => {
-    return (req, res, next) => {
-        res.status(200).json({ "message": "This is updateUser" })
-    };
-}
+// const updateUser = () => {
+//     return (req, res, next) => {
+//         res.status(200).json({ "message": "This is updateUser" })
+//     };
+// }
 
-const deleteUser = () => {
-    return (req, res, next) => {
-        res.status(200).json({ "message": "This is deleteUser" })
-    };
-}
+// const deleteUser = () => {
+//     return (req, res, next) => {
+//         res.status(200).json({ "message": "This is deleteUser" })
+//     };
+// }
 
 
 module.exports = {
-    getUserDetails,
-    updateUser,
-    deleteUser
+    getToken,
+    // updateUser,
+    // deleteUser
 }
